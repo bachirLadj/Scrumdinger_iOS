f346c49ea2cb547ce8f04d2594d11503bb67b697
bachirLadj <fb_ladjouzi@esi.dz>
2021-04-29 13:09:12 +0100

    passing data

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    @State private var scrums = DailyScrum.data
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ScrumsView(scrums: $scrums)
+            }
+        }
+    }
+}
